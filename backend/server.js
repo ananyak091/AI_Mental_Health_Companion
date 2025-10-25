2dd7832021f033ad5298d35298f793b91024e271
Ananya Kundu <ananyak091@gmail.com>
2025-10-25 22:03:21 +0530

    Day 3 plan is completed, Emotion Detection and reply generation is done in backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import { protect } from './middlewares/authMiddlewares.js';
+import aiRoutes from './routes/aiRoutes.js';
 
 
 dotenv.config(); //Load environment variables
@@ -14,6 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/ai', aiRoutes);
 
 
 app.get("/api/private", protect, (req, res) => {
